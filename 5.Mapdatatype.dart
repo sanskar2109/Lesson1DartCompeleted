2f8884363d8a45ad1163af7d5167183e244c70c4
Sanskar Singhal <89839415+sanskar2109@users.noreply.github.com>
2022-06-02 15:21:41 +0530

    Initial commit

---
+++ 5.Mapdatatype.dart
@@ -0,0 +1,12 @@
+// Map Data tpyes
+void main() {
+  Map data = { // I am interested in creating an object
+    "name": "Trashar",
+    "age": 20,
+    "address": 'Aligarh',
+    "Phoneno.": 123456789
+  };
+  print(data);
+  data["email"]= "Trashar@gmail.com";
+  print(data);
+}
